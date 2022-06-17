8d1c26ce2e2857bc82be34195e8e16a02585c47f
alex chika <mcluckey1@gmail.com>
2022-06-17 04:05:59 +0100

    updated profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -10,14 +10,62 @@ const Profile = () => {
   return (
     <ProfileWrapper>
       <section className="body">
-        <div>
-          <h2 className="mb30">
-            Hello <span>{currentUser.email}</span>
-          </h2>
-          <h3>Please I dont wanna miss the internship</h3>
-          <h3>Page under construction</h3>
-          <h3>still learnig databases to populate this page</h3>
+        <div className="container">
+          <div className="heading f">
+            <h1>
+              Welcome <span> {"Alex"}...</span>
+            </h1>
+            <div className="imgCon">
+              <img
+                className="fwh"
+                src={require("../Assets/girlaptop.png")}
+                alt=""
+              />
+            </div>
+          </div>
+          <div className="info mt20">
+            <h2>Info</h2>
+            <div className="names mt20">
+              <h3>
+                <span className="identifier">First Name :</span>
+                <span>{" Alex"}</span>
+              </h3>
+              <h3>
+                <span className="identifier">Second Name :</span>
+                <span>{" Chika"}</span>
+              </h3>
+            </div>
+          </div>
+          <div className="info mt20">
+            <h2>Contact</h2>
+            <div className="names mt20">
+              <h3>
+                <span className="identifier">Email :</span>
+                <span>{" mcluckey1@gmail.com"}</span>
+              </h3>
+              <h3>
+                <span className="identifier">Phone :</span>
+                <span>{" 09024783759"}</span>
+              </h3>
+            </div>
+          </div>
+          <div className="bio mt20">
+            <h2>Bio</h2>
+            <div className="names f mt20">
+              <h3>
+                <span className="identifier">About-Bio :</span> <br />
+                <span className="mt10">
+                  {" I am alex, a dev engineer and developer of javascript"}
+                </span>
+              </h3>
+              <h3>
+                <span className="identifier">D.O.B :</span> <br />
+                <span className="mt10">{" 2023 12 1999"}</span>
+              </h3>
+            </div>
+          </div>
         </div>
+        <button className="btn">Update profile</button>
       </section>
     </ProfileWrapper>
   );
@@ -31,8 +79,6 @@ const ProfileWrapper = styled.main`
   margin: 0 auto;
   color: white;
   .body {
-    display: grid;
-    place-items: center;
     width: 98%;
     margin: 0 auto;
     min-height: 90vh;
@@ -43,8 +89,66 @@ const ProfileWrapper = styled.main`
       rgba(255, 74, 0, 1) 35%,
       rgba(255, 155, 0, 1) 100%
     );
-    span {
+    .container {
+      max-width: 768px;
+      margin: 0 auto;
+      padding-left: 50px;
+    }
+    /* ,............., */
+    .heading {
+      height: 100px;
+      justify-content: space-between;
+      align-items: center;
+      padding: 60px 10px;
+      h1 {
+        font-size: 40px;
+      }
+      span {
+        color: teal;
+      }
+      .imgCon {
+        height: 100px;
+      }
+    }
+    .info,
+    .bio {
+      padding: 30px 0px;
+      border-bottom: 1px solid grey;
+      h2 {
+        text-decoration: underline;
+      }
+      .names {
+        .identifier {
+          display: inline-block;
+          color: grey;
+          font-size: 14px;
+          margin-right: 20px;
+          width: 7em;
+        }
+      }
+    }
+    .bio .names {
+      justify-content: space-between;
+      h3 {
+        width: 50%;
+        border-right: 1px solid grey;
+      }
+      h3:last-of-type {
+        border-left: 1px solid grey;
+        padding-left: 20px;
+      }
+      span {
+        display: inline-block;
+        width: 90%;
+      }
+    }
+    button {
+      display: block;
+      padding: 10px;
       color: teal;
+      text-align: center;
+      margin: 0 auto;
+      min-width: 7em;
     }
   }
   @media screen and (min-width: 768px) {
